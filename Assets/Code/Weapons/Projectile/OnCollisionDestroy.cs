c906381bc25baffa6740ea99ae5e4858de6e8a6c
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-09-30 02:56:29 +0200

    Collision Destroy

---
+++ Assets/Code/Weapons/Projectile/OnCollisionDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(WeaponProjectile))]
+public class OnCollisionDestroy : MonoBehaviour {
+
+	public LayerMask collisionLayer;
+
+	void OnCollisionEnter(Collision other) {
+		if (collisionLayer == (collisionLayer | (1 << other.gameObject.layer)))
+			Destroy(gameObject);
+	}
+}
