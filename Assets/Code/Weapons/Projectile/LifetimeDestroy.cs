ba3cd9bb09c9c1338e9090e9400cb91d67f0fd99
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-09-30 02:22:59 +0200

    Projectile Lifetime

---
+++ Assets/Code/Weapons/Projectile/LifetimeDestroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(WeaponProjectile))]
+public class LifetimeDestroy : MonoBehaviour, SimulateComponent {
+
+	public float duration = 1;
+
+	private float _lifeTime;
+
+	private WeaponProjectile _projectile;
+
+	void Awake() {
+		_projectile = GetComponent<WeaponProjectile>();
+	}
+
+	void Start() {
+		_lifeTime = 0;
+	}
+
+	void Update() {
+		_lifeTime += Time.deltaTime;
+		if (_lifeTime >= duration * _projectile.Modifiers.durationMultiplier)
+			Destroy(gameObject);
+	}
+
+	public void Simulate(float timeToSimulate) {
+		_lifeTime += timeToSimulate;
+	}
+}
